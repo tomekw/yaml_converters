18f49e382ca108603b9b0892f61b319173f30cdf
Tomek Wałkuski <ja@jestem.tw>
2012-07-25 19:08:54 +0200

    Convert simple two key-value pairs file

--- spec/yaml_to_segments_converter_spec.rb
+++ spec/yaml_to_segments_converter_spec.rb
@@ -8,4 +8,10 @@ describe YamlConverters::YamlToSegmentsConverter do
 
     its(:convert) { should eq({ 'key' => 'value' }) }
   end
+
+  context 'simple two key-value pairs file' do
+    let(:file_path) { 'spec/fixtures/two_key_value_pairs.yml' }
+
+    its(:convert) { should eq({ 'key1' => 'value1', 'key2' => 'value2' }) }
+  end
 end
