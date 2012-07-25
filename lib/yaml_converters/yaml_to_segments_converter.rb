127626c8ee65697ed7da1933cb5fbdb49f464ee3
Tomek Wałkuski <ja@jestem.tw>
2012-07-25 19:44:28 +0200

    Convert more complex files

--- lib/yaml_converters/yaml_to_segments_converter.rb
+++ lib/yaml_converters/yaml_to_segments_converter.rb
@@ -5,7 +5,7 @@ module YamlConverters
     end
 
     def convert
-      yaml_hash
+      @intermediate_flat_hash ||= flatten_hash(raw_yaml_hash)
     end
 
     private
@@ -14,8 +14,22 @@ module YamlConverters
       @file_contents ||= File.read(@file_path)
     end
 
-    def yaml_hash
-      Psych.load(file_contents)
+    def raw_yaml_hash
+      @raw_yaml_hash ||= Psych.load(file_contents)
+    end
+
+    def flatten_hash(hash, prefix = nil, result = {})
+      hash.each do |key, value|
+        current_prefix = prefix ? "#{prefix}.#{key}" : key
+
+        if !value.is_a?(Hash)
+          result[current_prefix] = value
+        else
+          flatten_hash(value, current_prefix, result)
+        end
+      end
+
+      result
     end
   end
 end
--- spec/yaml_to_segments_converter_spec.rb
+++ spec/yaml_to_segments_converter_spec.rb
@@ -14,4 +14,22 @@ describe YamlConverters::YamlToSegmentsConverter do
 
     its(:convert) { should eq({ 'key1' => 'value1', 'key2' => 'value2' }) }
   end
+
+  context 'nested key-value pair file' do
+    let(:file_path) { 'spec/fixtures/nested_key_value_pair.yml' }
+
+    its(:convert) { should eq({ 'parent.child' => 'value' }) }
+  end
+
+  context 'deeply ested key-value pairs file' do
+    let(:file_path) { 'spec/fixtures/deeply_nested_key_value_pairs.yml' }
+
+    its(:convert) do
+      should eq({
+        'grandmother.mother.daughter' => 'olga',
+        'grandmother.mother.sister' => 'ania',
+        'grandfather' => 'eugeniusz'
+      })
+    end
+  end
 end
