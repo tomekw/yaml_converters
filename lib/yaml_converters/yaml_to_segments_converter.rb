3175e19b18a16827e92d4c652fb24b3acdc8def3
Tomek Wałkuski <ja@jestem.tw>
2012-07-25 19:06:22 +0200

    Convert simple one key-value pair file

--- lib/yaml_converters/yaml_to_segments_converter.rb
+++ lib/yaml_converters/yaml_to_segments_converter.rb
@@ -1,7 +1,21 @@
 module YamlConverters
   class YamlToSegmentsConverter
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
     def convert
-      true
+      yaml_hash
+    end
+
+    private
+
+    def file_contents
+      @file_contents ||= File.read(@file_path)
+    end
+
+    def yaml_hash
+      Psych.load(file_contents)
     end
   end
 end
--- spec/yaml_to_segments_converter_spec.rb
+++ spec/yaml_to_segments_converter_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe YamlConverters::YamlToSegmentsConverter do
-  subject { described_class.new }
+  subject { described_class.new(file_path) }
 
-  its(:convert) { should eq true }
+  context 'simple one key-value pair file' do
+    let(:file_path) { 'spec/fixtures/one_key_value_pair.yml' }
+
+    its(:convert) { should eq({ 'key' => 'value' }) }
+  end
 end
